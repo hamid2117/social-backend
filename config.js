9ec5c2168c6ec836bedd7eab1a690d15895e5f2b
hamid2117 <saim008865@gmail.com>
2022-04-22 06:22:14 +0500

    updated db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://hamid2117:namsu9530@cluster0.czdmu.mongodb.net/first-merng?retryWrites=true&w=majority',
+    'mongodb+srv://hamid2117:hamid2117@cluster0.czdmu.mongodb.net/first-merng?retryWrites=true&w=majority',
   SECRET_KEY: 'SOMETHING secret ',
 }
